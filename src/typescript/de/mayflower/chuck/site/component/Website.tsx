6020edd4fd6154bff1ceccfd821b7c0b78fbf8ee
Christopher Stock <christopher.stock@mayflower.de>
2020-02-12 15:52:10 +0100

    Turned React Component class 'Website' into a function component.

--- src/typescript/de/mayflower/chuck/site/component/Website.tsx
+++ src/typescript/de/mayflower/chuck/site/component/Website.tsx
@@ -1,88 +1,41 @@
 
-    import * as React                                      from 'react';
-    import Layout                                          from 'antd/lib/layout';
-    import { Content, Debug, MainMenu, MenuItem, Setting } from '../..';
-
-    /** ****************************************************************************************************************
-    *   The React state for the Website component.
-    *******************************************************************************************************************/
-    export interface WebsiteState
-    {
-        /** The current selected menu item. */
-        currentMenuItem :MenuItem;
-    }
+    import * as React                               from 'react';
+    import Layout                                   from 'antd/lib/layout';
+    import { Content, MainMenu, MenuItem, Setting } from '../..';
 
     /** ****************************************************************************************************************
     *   The react component that represents the entire website.
     *******************************************************************************************************************/
-    export class Website extends React.Component<any, WebsiteState>
+    export const Website :() => JSX.Element = () :JSX.Element =>
     {
-        /** ************************************************************************************************************
-        *   Creates a new Website React component.
-        *
-        *   @param props The initial properties to assign to this component.
-        ***************************************************************************************************************/
-        public constructor( props:any )
-        {
-            super( props );
+        // create new state variable 'menuItem' with setter 'setMenuItem'
+        const [ menuItem, setMenuItem ] :[MenuItem, React.Dispatch<React.SetStateAction<MenuItem>>] = React.useState<MenuItem>( MenuItem.RANDOM_JOKE );
 
-            this.state = {
-                currentMenuItem: MenuItem.RANDOM_JOKE,
-            };
-        }
+        return <Layout>
 
-        /** ************************************************************************************************************
-        *   Being invoked every time this component renders.
-        *
-        *   @return The rendered JSX.
-        ***************************************************************************************************************/
-        public render() : JSX.Element
-        {
-            Debug.react.log( 'Website.render() being invoked' );
+            <MainMenu
+                onChangeMenu={ ( key:MenuItem ) :void => { setMenuItem( key ); } }
+            />
 
-            return <Layout>
+            <Layout>
 
-                <MainMenu
-                    onChangeMenu={ ( key:MenuItem ) :void => { this.onChangeMenu( key ); } }
+                <Layout.Header
+                    className="header"
+                >
+                    { Setting.TITLE }
+                </Layout.Header>
+
+                <Content
+                    currentSite={ menuItem }
                 />
 
-                <Layout>
+                <Layout.Footer
+                    className="footer"
+                >
+                    { Setting.COPYRIGHT }
+                </Layout.Footer>
 
-                    <Layout.Header
-                        className="header"
-                    >
-                        { Setting.TITLE }
-                    </Layout.Header>
+            </Layout>
 
-                    <Content
-                        currentSite={ this.state.currentMenuItem }
-                    />
-
-                    <Layout.Footer
-                        className="footer"
-                    >
-                        { Setting.COPYRIGHT }
-                    </Layout.Footer>
-
-                </Layout>
-
-            </Layout>;
-        }
-
-        /** ************************************************************************************************************
-        *   Being invoked when the selected menu index is changed.
-        *   This method is not invoked when the current active menu item is clicked again.
-        *
-        *   @param key The current selected key index in the menu.
-        ***************************************************************************************************************/
-        private onChangeMenu( key:MenuItem ) : void
-        {
-            Debug.major.log( 'Website.onChangeMenu to key [' + key + ']' );
-
-            this.setState(
-                {
-                    currentMenuItem: key,
-                }
-            );
-        }
-    }
+        </Layout>;
+    };
