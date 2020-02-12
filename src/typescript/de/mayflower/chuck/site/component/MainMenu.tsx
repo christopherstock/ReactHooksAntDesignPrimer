592cc4ecfc9c6f9a4caecbbd8ce5fdce4aa9667c
Christopher Stock <christopher.stock@mayflower.de>
2020-02-12 15:35:00 +0100

    Turned React Component class 'MainMenu' into a function component.

--- src/typescript/de/mayflower/chuck/site/component/MainMenu.tsx
+++ src/typescript/de/mayflower/chuck/site/component/MainMenu.tsx
@@ -4,12 +4,12 @@
     import Layout                       from 'antd/lib/layout';
     import Menu                         from 'antd/lib/menu';
     import { SelectParam }              from 'antd/lib/menu';
-    import { Debug, MenuItem, Setting } from '../..';
+    import { MenuItem, Setting } from '../..';
 
     /** ****************************************************************************************************************
     *   React properties for the MainMenu component.
     *******************************************************************************************************************/
-    export interface MenuProps
+    interface MenuProps
     {
         /** Being invoked when the selected menu item changes. */
         onChangeMenu: ( key:MenuItem ) => void;
@@ -18,78 +18,68 @@
     /** ****************************************************************************************************************
     *   The react component that represents the site menu.
     *******************************************************************************************************************/
-    export class MainMenu extends React.Component<MenuProps, any>
+    export const MainMenu : ( props:MenuProps ) => JSX.Element = ( props:MenuProps ) :JSX.Element =>
     {
-        /** ************************************************************************************************************
-        *   Being invoked every time this component renders.
-        *
-        *   @return The rendered JSX.
-        ***************************************************************************************************************/
-        public render() : JSX.Element
-        {
-            Debug.react.log( 'MainMenu.render() being invoked' );
+        return <Layout.Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                theme="dark"
+                className="sider"
+            >
 
-            return <Layout.Sider
-                    breakpoint="lg"
-                    collapsedWidth="0"
-                    theme="dark"
-                    className="sider"
-                >
+            <img
+                src={ Setting.PATH_IMAGE + 'logo.png' }
+                alt={ Setting.TITLE }
+                className="logo"
+            />
 
-                <img
-                    src={ Setting.PATH_IMAGE + 'logo.png' }
-                    alt={ Setting.TITLE }
-                    className="logo"
-                />
-
-                <Menu
-                    theme="dark"
-                    mode="inline"
-                    defaultSelectedKeys={ [ MenuItem.RANDOM_JOKE ] }
-                    onSelect={
-                        ( param:SelectParam ) :void => {
-                            this.props.onChangeMenu( param.key as MenuItem );
-                        }
+            <Menu
+                theme="dark"
+                mode="inline"
+                defaultSelectedKeys={ [ MenuItem.RANDOM_JOKE ] }
+                onSelect={
+                    ( param:SelectParam ) :void => {
+                        props.onChangeMenu( param.key as MenuItem );
                     }
-                >
+                }
+            >
 
-                    <Menu.Item key={ MenuItem.RANDOM_JOKE }>
-                        <Icon type="user" />
-                        <span>{ MenuItem.RANDOM_JOKE }</span>
-                    </Menu.Item>
+                <Menu.Item key={ MenuItem.RANDOM_JOKE }>
+                    <Icon type="user" />
+                    <span>{ MenuItem.RANDOM_JOKE }</span>
+                </Menu.Item>
 
-                    <Menu.Item key={ MenuItem.DOWNLOAD }>
-                        <Icon type="video-camera" />
-                        <span>{ MenuItem.DOWNLOAD }</span>
-                    </Menu.Item>
+                <Menu.Item key={ MenuItem.DOWNLOAD }>
+                    <Icon type="video-camera" />
+                    <span>{ MenuItem.DOWNLOAD }</span>
+                </Menu.Item>
 
-                    <Menu.Item key={ MenuItem.ABOUT }>
-                        <Icon type="upload" />
-                        <span>{ MenuItem.ABOUT }</span>
-                    </Menu.Item>
+                <Menu.Item key={ MenuItem.ABOUT }>
+                    <Icon type="upload" />
+                    <span>{ MenuItem.ABOUT }</span>
+                </Menu.Item>
 
-                    <Menu.Item key={ MenuItem.GO_PREMIUM }>
-                        <Icon type="user" />
-                        <span>{ MenuItem.GO_PREMIUM }</span>
-                    </Menu.Item>
+                <Menu.Item key={ MenuItem.GO_PREMIUM }>
+                    <Icon type="user" />
+                    <span>{ MenuItem.GO_PREMIUM }</span>
+                </Menu.Item>
 
-                    <Menu.SubMenu
-                        key={ MenuItem.EXTENDED }
-                        title={
-                        <span>
-                        <Icon type="appstore" />
-                        <span>{ MenuItem.EXTENDED }</span>
-                        </span>
-                    }>
+                <Menu.SubMenu
+                    key={ MenuItem.EXTENDED }
+                    title={
+                    <span>
+                    <Icon type="appstore" />
+                    <span>{ MenuItem.EXTENDED }</span>
+                    </span>
+                }>
 
-                        <Menu.Item key={ MenuItem.LEGAL   }>{ MenuItem.LEGAL   }</Menu.Item>
+                    <Menu.Item key={ MenuItem.LEGAL   }>{ MenuItem.LEGAL   }</Menu.Item>
 
-                        <Menu.Item key={ MenuItem.IMPRINT }>{ MenuItem.IMPRINT }</Menu.Item>
+                    <Menu.Item key={ MenuItem.IMPRINT }>{ MenuItem.IMPRINT }</Menu.Item>
 
-                    </Menu.SubMenu>
+                </Menu.SubMenu>
 
-                </Menu>
+            </Menu>
 
-            </Layout.Sider>;
-        }
-    }
+        </Layout.Sider>;
+    };
